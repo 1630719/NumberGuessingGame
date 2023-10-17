b7270a00dd4ae3b6fc7715c5ed56a0d5dc2b7f2a
rsfdfr <rererte@>
2023-10-17 13:33:53 -0400

    Fonctionel!

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ let  aleatoire = Math.ceil(Math.random()*100); //le nombre à deviner
 let compteur = 0
 let $essai = $("#essai")
 let $ListeItems = $("#ListeItems")
-let $Nombre = $("#" +$essai.val())
+let $ZoneMessage = $("#ZoneMessage")
+const $btn_nouvpartie = $("#NouvellePartie")
+
 $("h1").click(function ()
 {
     $(this).text("Reussi")
 })
-
 
 $("#BoutonSoumettre").click(function (){
     console.log(aleatoire)
@@ -15,41 +16,64 @@ $("#BoutonSoumettre").click(function (){
 
     //Incrémenter compteur de 1
     compteur++
-
+    let $Nombre = $("#" + $essai.val())
     if (compteur < 10 && $essai.val() !== aleatoire.toString())
     {
         //Ajouter l'essaie à la liste des nombres
-        $ListeItems.append($essai.val() + " ")
+        $ListeItems.append($essai.val() + (" "))
 
         // Si la tentative est inférieure au chiffre aléatoire
-        if ($essai.val() < aleatoire.toString())
-        {
-            //Retirer les nombres plus petits
+        if ($essai.val() < aleatoire.toString() && $essai.val() !== '100') {
+
+            //Retirer les nombres plus petits ET la tentative
+            $Nombre.prevAll().remove()
             $Nombre.remove()
+
+            //Afficher message
+            $ZoneMessage.text("Trop bas!")
+            $ZoneMessage.toggleClass("MauvaisGuess", true)
         }
 
+        else if ($essai.val() > aleatoire.toString() && $essai.val() !== '100')
+        {
+            // Retirer les nombres plus haut
+            $Nombre.nextAll().remove()
+            $Nombre.remove()
 
+            //Afficher message
+            $ZoneMessage.text("Trop haut!")
+            $ZoneMessage.toggleClass("MauvaisGuess", true)
+        }
+
+        else if ($essai.val() === '100')
+        {
+            $Nombre.remove()
+        }
     }
 
+    // Si 10 mauvais essais
     else if (compteur === 10 && $essai.val() !== aleatoire.toString())
 
-    {
+    {   //Afficher message de fin
         $ListeItems.text("Meilleure chance la prochaine fois!")
+        $btn_nouvpartie.removeClass("CacherBouton")
     }
 
+    //Si bonne réponse
     else if ($essai.val() === aleatoire.toString())
     {
-        $("#ListeItems").text("YOUPI")
+        //Message succès et afficher bouton recommencer
+        $ZoneMessage.text("Bravo!")
+        $ZoneMessage.toggleClass("BonGuess", true)
+        $btn_nouvpartie.removeClass("CacherBouton")
     }
 
+    //Pour vider le input quand on clique sur le bouton d'essai
+    $essai.val('')
+})
 
-
-
-    //Comparer valeur entrée aux nombres
-
-    // Si plus grand, retirer les plus gros nombres + message
-
-    //Si plus petit, retirer elems + petits + message
-
+$btn_nouvpartie.click(function()
+{
+    window.location.reload()
 })
 
